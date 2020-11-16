c0b24de345156a653c0c261e389c065848412981
Tito Nobre <titonobre@gmail.com>
2020-11-16 02:24:49 +0000

    Fix Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import '../styles/globals.css';
 
-import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css'
-
+import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css';
 
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import type { AppProps /*, AppContext */ } from 'next/app';
 
+import Div100vh from 'react-div-100vh';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<React.Fragment>
 			<CssBaseline />
-			<Component {...pageProps} />
+			<Div100vh>
+				<Component {...pageProps} />
+			</Div100vh>
 		</React.Fragment>
 	);
 }
