830ebd982797414708b281f2d5b8bd5ed423b3f2
Tito Nobre <titonobre@gmail.com>
2020-11-29 17:39:01 +0000

    fix pwa config

--- next.config.js
+++ next.config.js
@@ -6,4 +6,21 @@ const withPlugins = require('next-compose-plugins');
 const reactSvg = require('next-react-svg');
 const pwa = require('next-pwa');
 
-module.exports = withPlugins([[reactSvg, { include: path.resolve(__dirname, 'images') }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]], [pwa]], {});
+module.exports = withPlugins(
+	[
+		[
+			reactSvg,
+			{
+				include: path.resolve(__dirname, 'images'),
+			},
+			[PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD],
+		],
+		[pwa],
+	],
+	{
+		pwa: {
+			disable: process.env.NODE_ENV !== 'production',
+			dest: 'public',
+		},
+	},
+);
