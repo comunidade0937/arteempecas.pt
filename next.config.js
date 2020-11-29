d42469a735d83ca79f28f802887a504fd40ec8c5
Tito Nobre <titonobre@gmail.com>
2020-11-29 17:14:48 +0000

    add next-pwa

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/const
 
 const withPlugins = require('next-compose-plugins');
 const reactSvg = require('next-react-svg');
+const pwa = require('next-pwa');
 
-// module.exports = withPlugins([
-//     withSvgr
-//     // your other plugins here
-//   ]);
-
-module.exports = withPlugins([[reactSvg, { include: path.resolve(__dirname, 'images') }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]]], {
-	//basePath: '/tito/aep2020',
-	// trailingSlash: true,
-});
-
-// module.exports = reactSvg({
-// 	include: path.resolve(__dirname, 'images'),
-// 	webpack(config, options) {
-// 		return config;
-// 	},
-// });
+module.exports = withPlugins([[reactSvg, { include: path.resolve(__dirname, 'images') }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]], [pwa]], {});
