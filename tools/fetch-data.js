e8fd9bb08da38d252b3b96ec1a9f56734c47fc58
Tito Nobre <titonobre@gmail.com>
2020-11-29 01:07:29 +0000

    update fetch script

--- tools/fetch-data.js
+++ tools/fetch-data.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fsp = require('fs').promises;
+const hashSum = require('hash-sum');
 
 const url = 'https://spreadsheets.google.com/feeds/cells/1SbqtiD7tJbH45dW82_eA6EZ79q_ogWeUSWQTK7ia6lk/2/public/values?alt=json';
 
@@ -22,12 +23,15 @@ function parseFeed(feed) {
 		const markers = parseFeed(response.data.feed)
 			.slice(1)
 			.filter((row) => !!row[2])
-			.map((row) => ({
-				id: row[3],
-				name: row[0],
-				address: row[1],
-				coordinates: row[2].split(',').map((s) => Number.parseFloat(s.trim())),
-			}))
+			.map((row) => {
+				const name = row[0];
+				const address = row[1];
+				const coordinates = row[2].split(',').map((s) => Number.parseFloat(s.trim()));
+				const flyer = row[3];
+				const description = row[4];
+				const id = hashSum(coordinates.join(','));
+				return { id, flyer, name, address, description, coordinates };
+			})
 			.sort((a, b) => a.id - b.id);
 
 		await fsp.writeFile('public/markers.json', JSON.stringify(markers, undefined, 4), 'utf8');
