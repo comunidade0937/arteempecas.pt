45859910bc7d38af227f4298c8cb7fb9bd1a1842
Tito Nobre <titonobre@gmail.com>
2020-12-01 23:38:08 +0000

    cleanup scan page

--- pages/scan.tsx
+++ pages/scan.tsx
@@ -1,24 +1,17 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
-
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
-import { MyMarker } from '../components/map/Map';
 import { BottomBar } from '../components';
 
 import layout from '../styles/Layout.module.css';
 
-type MapProps = {
-	markers?: Array<MyMarker>;
-};
-
 const DynamicQRReader = dynamic(() => import('../components/qr-reader/qr-reader'), { ssr: false });
 
-export default function Demo({ markers }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Demo() {
 	return (
 		<div className={layout.container}>
 			<Head>
@@ -42,15 +35,3 @@ export default function Demo({ markers }: InferGetStaticPropsType<typeof getStat
 		</div>
 	);
 }
-
-export const getStaticProps: GetStaticProps<MapProps> = async (context) => {
-	const data = await import('../public/markers.json');
-
-	const markers: Array<MyMarker> = data.default as Array<MyMarker>;
-
-	return {
-		props: {
-			markers,
-		},
-	};
-};
